04cbbc049342e8170684d95a1a4dd454b481d4a6
David Reyes <yandridavidr@gmail.com>
2024-01-31 18:38:28 -0500

    created move script and setup animator controller

---
+++ Assets/Game/Scripts/Character.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    // Variables
+    private CharacterController _cc;
+    private PlayerInput _playerInput;
+    private Animator _animator;
+    private Vector3 _moveVelocity;
+    public float moveSpeed = 5f;
+    private float verticalVelocity;
+    public float gravity;
+    
+    // Awake is called when the script instance is being loaded.
+    protected void Awake() {
+        _cc = GetComponent<CharacterController>();
+        _playerInput = GetComponent<PlayerInput>();
+        _animator = GetComponent<Animator>();
+    }
+
+    private void CalculatePlayerMovement() {
+        _moveVelocity.Set(_playerInput.horizontalInput, 0, _playerInput.verticalInput);
+        _moveVelocity.Normalize();
+        _moveVelocity = Quaternion.Euler(0, -45f, 0) * _moveVelocity;
+        // Run animation
+        _animator.SetFloat("Speed",_moveVelocity.magnitude);
+        
+        _moveVelocity *= moveSpeed * Time.deltaTime;
+        // rotación del jugador
+        if (_moveVelocity != Vector3.zero) {
+            transform.rotation = Quaternion.LookRotation(_moveVelocity);
+        }
+        // Fall animation
+        _animator.SetBool("AirBorne", !_cc.isGrounded);
+    }
+    
+    // This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+    protected void FixedUpdate() {
+        
+        CalculatePlayerMovement();
+        
+        if (_cc.isGrounded == false){
+            verticalVelocity = gravity;
+        }
+        else {
+            verticalVelocity = gravity *0.3f;
+        }
+        _moveVelocity += verticalVelocity * Vector3.up * Time.deltaTime;
+        
+        _cc.Move(_moveVelocity);
+    }
+}
---
+++ Assets/Game/Scripts/PlayerInput.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    //Variables
+    public float horizontalInput;
+    public float verticalInput;
+    
+    // Update is called once per frame
+    void Update() {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+    
+    // This function is called when the behaviour becomes disabled () or inactive.
+    protected void OnDisable() {
+        horizontalInput = 0;
+        verticalInput = 0;
+    }
+}
