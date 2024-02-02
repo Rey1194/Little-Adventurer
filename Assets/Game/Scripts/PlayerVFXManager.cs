be04a79b243395c3b5ed4e37f0999f2a6baa4bdc
David Reyes <yandridavidr@gmail.com>
2024-02-01 19:42:11 -0500

    added player VFX footstep

---
+++ Assets/Game/Scripts/PlayerVFXManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class PlayerVFXManager : MonoBehaviour
+{
+    public VisualEffect Player_FootStep;
+    
+    public void Update_FootStep(bool state) {
+        if (state) {
+            Player_FootStep.Play();
+        }
+        else{
+            Player_FootStep.Stop();
+        }
+    }
+}
